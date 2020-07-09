7a0a1216fe7df8e7e00566515ab46d9e60ad307e
HiiZun <HiiZun@protonmail.com>
2020-07-09 15:11:50 +0200

    adding customizable  number of processes

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
 // We will start multiple processes with child process to do more stress on the cpu :D 
-const { spawn } = require('child_process');
-
+var exec = require('child_process').exec;
+var state = 'node ./stresser.js'
+var conf = require("./config.json")
+var os = require("os")
 console.warn("⚠⚠ | WARNING ! WE ARE NOT RESPONSABLE IF YOUR COMPUTER CRASH OR EXPLODE (no it's not possible haha) BUT DON'T USE THIS TO MALICIOUS ENDS | ⚠⚠")
 
+var processestodo
+
+if(conf.processes){
+  if(isNaN(conf.processes)){
+    if(conf.processes === "auto"){
+      processestodo = os.cpus.length + 1
+    } else {
+      processestodo = os.cpus.length + 1
+    }
+  } else {
+    if(conf.processes <= 0){
+      processestodo = os.cpus.length + 1
+    } else {
+      processestodo = conf.processes
+    }
+  }
+} else {
+  processestodo = os.cpus.length + 1
+
+}
 
 
-while (true) {
-    const ls = spawn('node', ['./stresser.js']);
-    ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
+
+
+
+
+
+for(var i = 0; i < processestodo ; i++){
+
+    (function(i){
+      var child = exec(state);
+
+
+
+      // Listen if the process closed
+      child.on('close', function(exit_code) {
+          console.log('Closed before stop: Closing code: ', exit_code);
+          if(require("os").platform() !== "win32"){
+          exec("killall node")
+          } else {
+            exec("taskkill /F /IM nodes")
+          }
       });
-      
-      ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      
-      ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
+  })(i)
+
 }
 
--- stresser.js
+++ stresser.js
@@ -1,3 +1,6 @@
 while(true){
     console.log("oh yeah, your computer is getting some lags, it's not our problem !")
+    console.log(require("os").arch())
+    console.log(require("os").freemem())
+    console.log(require("os").platform())
 }
